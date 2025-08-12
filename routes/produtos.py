4efcd8609769f52773a3506588c38694b53ac6f9
Gustavo Higuti <gustavo.france@gmail.com>
2025-08-12 20:46:41 -0300

    new system

--- routes/produtos.py
+++ routes/produtos.py
@@ -1,11 +1,7 @@
 from flask import Blueprint, render_template
 
-produtos_route = Blueprint('produtos', __name__, url_prefix='/produtos')
+produtos_route = Blueprint('produtos', __name__)
 
-@produtos_route.route('/')
+@produtos_route.route('/produtos')
 def produtos():
     return render_template('produtos.html')
-
-@produtos_route.route('/estoque')
-def estoque():
-    return render_template('estoque.html')
